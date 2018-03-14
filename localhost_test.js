091d440a67679c448d5b09a9bcc97482afb65ed9
sybren marechal <sybrenmarechal@icloud.com>
2018-03-14 11:25:36 +0100

    add a localhost_test.js to run in a diffrent application to test the project

---
+++ localhost_test.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+   // console.log(req.body.toString());
+    res.end();
+    console.log("data van de url")
+    console.log(req.url);
+
+
+
+    let body = [];
+req.on('data', (chunk) => {
+  body.push(chunk);
+}).on('end', () => {
+  body = Buffer.concat(body).toString();
+  // at this point, `body` has the entire request body stored in it as a string
+  console.log(body);
+});
+
+
+
+}).listen(8000);
